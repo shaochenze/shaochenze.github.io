5e8e56f247b9dcc4acfbd7a23764d9b83d13749b
shaochenze <42638348+shaochenze@users.noreply.github.com>
2025-10-27 04:17:45 +0000

    Deploying to gh-pages from @ shaochenze/shaochenze.github.io@edeaca094b8154f4591ad51a43323edbbc0b0737 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,7 +72,18 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-a-post-with-plotly-js",
+            },{id: "post-",
+        
+          title: "",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/2025-10-27-CALM/";
+          
+        },
+      },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
